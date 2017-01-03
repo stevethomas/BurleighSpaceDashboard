209b9b8edc1903ed2c2e3848d819d39f081d7129
Steve Thomas <steve@thomasmultimedia.com.au>
2017-01-03 12:05:01 +1000

    allow IP range in IP whitelist

--- app/Http/Middleware/IpWhitelist.php
+++ app/Http/Middleware/IpWhitelist.php
@@ -16,16 +16,40 @@ class IpWhitelist
      */
     public function handle($request, Closure $next)
     {
-        if (!in_array($request->ip(), [
-            env("IP_WHITELIST"),
-            '127.0.0.1',
-        ])
-        ) {
+        if (!$this->ipIsAllowed($request)) {
             return response('Unauthorized.', 401);
         }
 
+        // login as user 1 so private broadcasting works
         Auth::loginUsingId(1);
 
+        // carry on
         return $next($request);
+    }
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     * @return bool
+     */
+    private function ipIsAllowed($request)
+    {
+        if ($request->ip() == '127.0.0.1') {
+            // always allow localhost
+            return true;
+        }
+
+        $whitelist =  env("IP_WHITELIST");
+
+        if (!is_null($whitelist)) {
+            if (ends_with($whitelist, '*') and starts_with($request->ip(), substr($whitelist, 0, -1))) {
+                // accept an IP range
+                return true;
+            } else {
+                // exact match
+                return $request->ip() == $whitelist;
+            }
+        }
+
+        return false;
     }
 }
